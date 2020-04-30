9f4ed62307f4928e57aa4644bd2e82c27d909aca
emranblue <emran.blue.120@gmali.com>
2020-04-30 17:52:05 +0600

    graph added

--- newton-forward-difference.py
+++ newton-forward-difference.py
@@ -1,18 +1,17 @@
 #newton-forward_difference method
 from numpy import *
-import matplotlib.pyplot as plt
 import math as mt
+import matplotlib.pyplot as plt
+plt.style.use('dark_background')
 x=ndarray((19,20),dtype='float')
 i=0
 for line in loadtxt('ndf_input.txt'):
     x[i,0],x[i,1]=line
     i+=1
-x1=27
 a=8
 h=1
-u=(x1-a)/h
-def coff(n):
-    j=0
+def coff(n,x1):
+    u=(x1-a)/h
     p=1
     for i in range(n):
         p=p*(u-i)
@@ -22,17 +21,23 @@ for i in range(2,20):
         x[j,i]=x[j+1,i-1]-x[j,i-1]
 
 fl=open('ndf_output.txt','w')
-result=0
-for i in range(19):
-    fl.write('%6.1f   '%(x[i,0]))
-    for j in range(1,20-i):
-        fl.write('%5.2f    '%(x[i,j]))
-    fl.write('\n')
-    result=result+coff(i)*x[0,i+1]
-fl.write('result is %5.2f'%(result))
-print(result)
-fl.close()
+def f(x1):
+    result=0
+    for i in range(19):
+        fl.write('%6.1f   '%(x[i,0]))
+        for j in range(1,20-i):
+            fl.write('%5.2f    '%(x[i,j]))
+        fl.write('\n')
+        result=result+coff(i,x1)*x[0,i+1]
+    return result    
 plt.plot(x[:,0],x[:,1])
+q=linspace(10,20,1000)
+w=array(f(q))
+plt.xlim(9,20)
+plt.ylim(0,800)
+plt.plot(q,w)
+plt.savefig('newton-forward.png')
 plt.show()
-
+fl.write('result is %5.2f'%(w[-1]))
+fl.close()
 
